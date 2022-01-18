36867c0eda032c6f06676789a60cdbe92542674d
Eric <erics1337@gmail.com>
2022-01-18 11:28:40 -0700

    Restart

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,8 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-
 function Footer() {
     return (
-        <footer>
-            <Container>
-                <Row>
-                    <Col className="text-center py-3">Copyright &copy; ProShop</Col>
-                </Row>
-            </Container>
-        </footer>
+        <div>
+            
+        </div>
     )
 }
 
